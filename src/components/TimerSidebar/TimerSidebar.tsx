321b8846e896536c2b0870d50005e59cd6f58bd1
riddlew <riddle.w@gmail.com>
2023-06-06 03:36:54 -0400

    Scroll to selected when out of container bounds

--- src/components/TimerSidebar/TimerSidebar.tsx
+++ src/components/TimerSidebar/TimerSidebar.tsx
@@ -9,15 +9,32 @@ export default function TimerSidebar() {
 	const timers = useAppSelector(selectTimerQueue);
 	const currentIdx = useAppSelector(selectRunningIdx);
 	const timerRef = useRef<React.RefObject<HTMLLIElement>[]>([]);
+	const sidebarRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		timerRef.current[currentIdx].current?.scrollIntoView({
-			behavior: "smooth",
-		});
-	}, [currentIdx, timerRef]);
+		const selectedLi = timerRef.current[currentIdx]?.current;
+		if (!selectedLi || !sidebarRef.current) return;
+
+		const liBounds = selectedLi.getBoundingClientRect();
+		const sidebarBounds = sidebarRef.current.getBoundingClientRect();
+
+		if (liBounds.bottom > sidebarBounds.bottom - 30) {
+			selectedLi.scrollIntoView({
+				block: "start",
+				behavior: "smooth",
+			});
+		}
+
+		if (liBounds.top < sidebarBounds.top + 30) {
+			selectedLi.scrollIntoView({
+				block: "end",
+				behavior: "smooth",
+			});
+		}
+	}, [currentIdx]);
 
 	return (
-		<SC.TimerSidebar>
+		<SC.TimerSidebar ref={sidebarRef}>
 			<SC.TimerList>
 				{timers.map((timer, i) => {
 					timerRef.current[i] = createRef<HTMLLIElement>();
